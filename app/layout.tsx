22afa5f9deb04bc33a9788d3e34fd098b4600e81
Murilo Vanucci <murilo.vanucci@hyperdev.com.br>
2024-07-13 21:40:10 -0300

    First commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import '@/app/ui/global.css'
+import { inter } from './ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="pt-BR">
+      <body className={`${inter.className} antialased`}>{children}</body>
     </html>
   );
 }
